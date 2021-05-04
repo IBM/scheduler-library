626ea50f7531b234ae82224a18d63ff5a49075bb
J-D Wellman <wellman@us.ibm.com>
2021-05-04 12:17:32 -0400

    Removed unused MAX_TASK_TYPES (made redundant some time ago)

--- sched_lib/include/scheduler.h
+++ sched_lib/include/scheduler.h
@@ -64,7 +64,6 @@ typedef enum { TASK_FREE = 0,
 #define MAX_ACCEL_DESC_LEN   256
 
 // These are fields defined by the application when it gets/sets up a new cheduler datastate block
-//#define MAX_TASK_TYPES     32
 #define MAX_ACCEL_TYPES    4
 #define GLOBAL_METADATA_POOL_BLOCKS 32
 #define MAX_TASK_TIMING_SETS   16
