ac461042453c072d76f0f46e711f1b06c809e8c8
J-D Wellman <wellman@us.ibm.com>
2021-06-17 18:09:38 -0400

    Adding some run-scripts to show how I invoked this... in concert with a socket/wifi1 and 2 of course.

---
+++ examples/demo-me/scripts/do_t9A_car2.sh
@@ -0,0 +1,1 @@
+./car2-scheduler-W-S-P3V0F0N0 -G config_files/base_me_p2.config -A -gS -l RL -o -s500 -t traces/t9A_r"$1"_5k.tr -f0 -R traces/norm_radar_01k_by_5_dictionary.dfn >& res_car2_A_t9A_r"$1"_500.out_1
