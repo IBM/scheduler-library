cc0c2c12d3c1b979ca0c67a4fe2476a2c89791dc
J-D Wellman <wellman@us.ibm.com>
2021-06-21 09:33:45 -0400

    Added input parm to run length

--- examples/demo-me/scripts/do_t9_car2.sh
+++ examples/demo-me/scripts/do_t9_car2.sh
@@ -1,1 +1,1 @@
-./car2-scheduler-W-S-P3V0F0N0 -G config_files/base_me_p2.config -gS -l RL -o -s500 -t traces/t9_r"$1"_5k.tr -f0 -R traces/norm_radar_01k_by_5_dictionary.dfn >& res_car2_t9_r"$1"_500.out_1
+./car2-scheduler-W-S-P3V0F0N0 -G config_files/base_me_p2.config -l RL -o -s$1 -t traces/t9_r"$2"_5k.tr -f0 -R traces/norm_radar_01k_by_5_dictionary.dfn >& res_car2_t9_r"$2"_$1.out
